b94139b7a1ed0bc4bf60bf8d4365aef25e0779ed
Michael Dominick <michael@themadbotter.com>
2025-11-07 15:35:57 -0500

    adding DuckDB Init

---
+++ duckdb_init.sql
@@ -0,0 +1,6 @@
+SET s3_endpoint = 'minio:9000';
+SET s3_url_style = 'path';
+SET s3_use_ssl = false;
+SET s3_region = 'us-east-1';
+SET s3_access_key_id = 'minioadmin';
+SET s3_secret_access_key = 'minioadmin';
